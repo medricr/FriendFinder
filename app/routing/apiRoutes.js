d17d542c01ddbdf92bb816985855191ef882ffff
Medric Riley <medricr@gmail.com>
2019-06-16 22:44:12 -0700

    changed pseudocode, added package.json

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,6 +9,16 @@ module.exports = function(app){
     })
 
     app.post('/api/friends',function(req,res){
-        
+        /*
+        get user input data
+
+        parse out the users scores
+
+        compare them to the scores of everyone else in the friend_list array
+
+        determine which of the stored profiles is the best for the user
+
+        display that profile in a modal to the user
+         */
     })
 }
